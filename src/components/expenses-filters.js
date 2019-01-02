f5a13adfbb85076b3bdf7d293c1859ce57d19134
Stefano <stefan.emailacct@gmail.com>
2019-01-02 18:44:56 +0000

    [search-bar]updated search-bar styling and created component

--- src/components/expenses-filters.js
+++ src/components/expenses-filters.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import '../scss/components/expenses-list.scss';
 import {connect} from 'react-redux';
-import {setTextFilter} from '../actions/filters';
+import SearchBar from './search-input';
 
 
 const ExpensesFilters = (props) => (
   <div className='input-filters'>
     <div className='filters-container'>
-      <input type='text' placeholder='enter search term'
-       value={props.filters.text}
-       onChange={
-        (e) => {
-          props.dispatch(setTextFilter({
-            text: e.target.value
-          }));
-          //console.log(e.target.value);
-          //console.log(props);
-        }}/>
+      <SearchBar />
     </div>
   </div>
 
