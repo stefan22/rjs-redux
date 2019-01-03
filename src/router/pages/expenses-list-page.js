b9d3e778e2cb89a5c7ccf53651bf808bfc0c4b04
Stefano <stefan.emailacct@gmail.com>
2019-01-03 23:41:29 +0000

    [style]removed class

--- src/router/pages/expenses-list-page.js
+++ src/router/pages/expenses-list-page.js
@@ -7,12 +7,12 @@ import ExpensesFilters from '../../components/expenses-filters';
 class ExpensesListPage extends Component {
   render() {
     return (
-      <div className='container no-padding'>
+      <div className='container'>
         <header className='header expenses-list'>
           <h1 className='text-center'>Expenses List page</h1>
           <p className='text-center'>(filters off description)</p>
         </header>
-          <div className="main-content full-container">
+          <div className="main-content">
             <ExpensesFilters />
             <ExpensesList />
           </div>
