7a720e5d0589f670bb9afbb74c8f869f7c1e2a96
Stefano <stefan.emailacct@gmail.com>
2019-01-08 18:41:59 +0000

    [movie-info]added info component

--- src/components/table-info.js
+++ src/components/table-info.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import '../scss/components/movies-table.scss';
 
 
-const TableInfo = (props) => (
-  <div className='table-info'>
-    <ul>
-      <li>Press enter to clear input field</li>
-      <li>Click on table cell to store/highlight item</li>
-      <li>Click on table cell again to remove stored item</li>
-    </ul>
-  </div>
+const TableInfo = (props) => {
+  const bull = <span className='bullet'>•</span>;
+  return (
+    <Card className='table-info'>
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          Movie legend
+        </Typography>
+        <Typography component="ul">
+          <li>{bull} Press enter to clear input field</li>
+          <li>{bull} Click on table cell to store/highlight item</li>
+          <li>{bull} Click on table cell again to remove stored item</li>
 
-);
+        </Typography>
+      </CardContent>
+    </Card>
+
+  );
+
+};
 
 
 
 export default TableInfo;
+
+
+
